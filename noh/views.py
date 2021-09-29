ab4cc1f3b878ba50b2f534467e2a0b6ab70fcdbf
himi19 <hiroshi131225@gmail.com>
2021-09-29 14:12:57 +0900

    totorial: try django <3>

--- noh/views.py
+++ noh/views.py
@@ -1,22 +1,46 @@
+from django import template
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, response, Http404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'noh/index.html', context)
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'noh/detail.html', {'question': question})
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'noh/results.html', {'question': question})
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'noh/index.html', context)
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'noh/detail.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'noh/results.html', {'question': question})
+
+class IndexView(generic.ListView):
+    template_name = 'noh/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Reutrn the last five published questions(
+        not including those set to be published in the future)."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'noh/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
